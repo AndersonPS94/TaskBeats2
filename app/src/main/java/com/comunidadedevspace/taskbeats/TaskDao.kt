ba37bf2810596254f97407654d068ead9523154d
AndersonPS94 <dson_adn@hotmail.com>
2024-04-08 19:51:34 -0300

    initial commit

---
+++ app/src/main/java/com/comunidadedevspace/taskbeats/TaskDao.kt
@@ -0,0 +1,27 @@
+package com.comunidadedevspace.taskbeats
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+
+@Dao
+interface TaskDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(task: Task)
+
+    @Query("Select * from task")
+    fun getAll(): List<Task>
+
+    @Update(onConflict = OnConflictStrategy.REPLACE)
+    fun update(task: Task)
+
+    //Delete encontrar por ID
+    @Query("DELETE from task")
+    fun deleteAll()
+
+    //Deletando pelo ID
+    @Query("Delete from task where id =:id")
+    fun deleteById(id: Int)
+}
